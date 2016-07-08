f45c9a23ceb2aa7385da9029b583b82bb6cb45a9
Synisysm <synic@synical.de>
2016-07-08 17:47:38 +0200

    Add hashes to ScoreSyncMessage

--- src/main/java/umundo/model/ScoreSyncMessage.java
+++ src/main/java/umundo/model/ScoreSyncMessage.java
@@ -4,20 +4,23 @@ package umundo.model;
 import javafx.beans.binding.StringBinding;
 import org.umundo.core.Message;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ScoreSyncMessage {
 
-    private Map<String, String> matches;
-    private Map<String, String> players;
+    private Map<String, String> matches; // contains machUID -> winnerUID mapping
+    private Map<String, String> players; // contains playerUID -> playerName mapping for players contained in matches
+    private byte[][] hashes; // optional, for piggybacking hashes of prefixed areas
 
     private String prefix;
 
-    public ScoreSyncMessage(String prefix, Map<String, String> matches, Map<String, String> players) {
+    public ScoreSyncMessage(String prefix, Map<String, String> matches, Map<String, String> players, byte[][] hashes) {
         this.prefix = prefix;
         this.matches = matches;
         this.players = players;
+        this.hashes = hashes;
     }
 
     public Message get() {
@@ -49,6 +52,18 @@ public class ScoreSyncMessage {
         }
         m.putMeta("players", sb.toString().substring(0, sb.length() - 2));
 
+        // build hashes string
+        if(hashes == null) {
+            return m;
+        }
+
+        sb.setLength(0);
+        for (int i = 0; i < 17; i++) {
+            sb.append(DatatypeConverter.printHexBinary(hashes[i]));
+            sb.append(",");
+        }
+        m.putMeta("hashes", sb.toString().substring(0, sb.length() - 2));
+
 
         return m;
     }
@@ -73,7 +88,21 @@ public class ScoreSyncMessage {
             players.put(data[0], data[1]);
         }
 
-        return new ScoreSyncMessage(m.getMeta("prefix"), matches, players);
+        // parse hashes
+        String hashesString = m.getMeta("hashes");
+        if(hashesString == null) {
+            return new ScoreSyncMessage(m.getMeta("prefix"), matches, players, null);
+        }
+
+        String[] hashesStringArray = m.getMeta("hashes").split(",");
+        byte[][] hashes = new byte[17][];
+        if (hashesStringArray.length != 18) return null;
+        for (int i = 0; i < 17; i++) {
+            hashes[i] = DatatypeConverter.parseHexBinary(hashesStringArray[i]);
+        }
+
+        return new ScoreSyncMessage(m.getMeta("prefix"), matches, players, hashes);
+
     }
 }
 
